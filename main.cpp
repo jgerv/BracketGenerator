5fc75df50e2d94667a91b9f5b1944463fc433441
user <user@localhost.localdomain>
2019-10-28 17:44:30 -0500

    working on rounds of bracket

--- main.cpp
+++ main.cpp
@@ -14,21 +14,29 @@ int findNumberByes(int number)
 {
     int countUp, countDown = 0;
     if(isPowerOfTwo(number)){
-        return number; 
+        return 0; 
     }
     else{
 	while(!isPowerOfTwo(number + countUp) && !isPowerOfTwo(number - countDown)){
-            countUp++;
-            countDown++;
-            if(isPowerOfTwo(number + countUp)){
-                return countUp;
-            }
-            else if(isPowerOfTwo(number - countDown)){
-                return countDown;
-            }
+	    countUp++;
+	    countDown++;
+	    if(isPowerOfTwo(number + countUp)){
+	        return countUp;
+	    }
+	    else if(isPowerOfTwo(number - countDown)){
+	        return countDown;
+	    }
 	}
     }
     return 0;
+}
+
+int findNumRounds(int number)
+{
+    if(isPowerOfTwo(number))
+	return log(number) / log(2);
+    else
+        return log(number) / (log(2)) + 1;
 }
 
 int main(){
@@ -38,19 +46,32 @@ int main(){
     int numSets = 0; //number of sets to be played in current round
     int currRound = 1; //current round the bracket is in
     int numByes = 0;
+    int numRounds = 0;
+    vector<vector<string>> rounds;
 
     cout << "How many entrants will there be? ";
     cin >> numEntrants;
 
-    for(int i = 0; i < numEntrants; i++){
-        cout << "Seed " << i + 1 << ": ";
-        cin >> placeholder;
-        nameEntrants.push_back(placeholder);
-    }
-
     numByes = findNumberByes(numEntrants);
     numSets = (numEntrants - numByes) / 2;
-    cout << numSets << "in the " << currRound << "to be played" << endl;
-    
+    numRounds = findNumRounds(numEntrants);
+    cout << numRounds << endl;
+    cout << numByes << endl;
+    cout << numSets << " sets in the " << currRound << " round to be played" << endl;
+
+    for(int i = 0; i < numEntrants; i++)
+    {
+        cout << "Seed " << i + 1 << ": ";
+	cin >> placeholder;
+	nameEntrants.push_back(placeholder);
+    }
+
+    for(int i = 0; i < numEntrants; i++)
+    {
+	vector<string> myVec;
+	myVec.push_back(nameEntrants[i]);
+	rounds.push_back(myVec);
+    }
+
     return 0;
 }
